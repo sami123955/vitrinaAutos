5166f344f1c6f774b113fb84cfaf4688cc1f69a4
davidsanta1225 <santamariavalenciadavid@gmail.com>
2018-10-07 20:35:05 -0500

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+import { Inicio } from './controllers/Inicio';
+import { CompararAutos } from './controllers/CompararAutos';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Inicio,
+    CompararAutos
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
